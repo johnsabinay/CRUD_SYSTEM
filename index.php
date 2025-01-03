7973c208a8ac096bb14277b559d0b3eda4e27099
John Sabinay <sabinayjl20@gmail.com>
2025-01-03 23:56:49 +0800

    initial table. fetch data from sql

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD SYSTEM</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+    <button>ADD</button>
+    <div class="container">
+    <table class="table">
+        <thead>
+                <tr>
+                    <th scope="col">ID </th>
+                    <th scope="col">NAME </th>
+                    <th scope="col">AGE</th>
+                    <th scope="col">EMAIL </th>
+                    <th scope="col">CONTACTNO </th>
+                    <th scope="col">BIRTHDATE </th>
+                    <th scope="col"> POSITION</th>
+                    <th scope="col"> ACTION</th>
+
+                </tr>
+             
+
+                <?php
+                $hostName="localhost";
+                $dbUser="root";
+                $dbPass="";
+                $dbName="employee_db";
+                $conn = mysqli_connect($hostName, $dbUser,$dbPass,$dbName);
+                if (!$conn) {
+                    die("Connection is not successful");
+                }
+                $sql = "SELECT * FROM employee_tb";
+                $result = mysqli_query($conn,$sql);
+                while ($row = mysqli_fetch_array($result)) {
+                    $id=$row["ID"];
+                    $name=$row["NAME"];
+                    $age=$row["AGE"];
+                    $email=$row["EMAIL"];
+                    $contact=$row["CONTACTNO"];
+                    $bday=$row["BIRTHDATE"];
+                    $position=$row["POSITION"];
+
+
+                    echo "<tr>
+                    <th scope ='row'>$id</th>
+                    <td>$name</td>
+                    <td>$age</td>
+                    <td>$email</td>
+                    <td>$contact</td>
+                    <td>$bday</td>
+                    <td>$position</td>
+                    <td><button>EDIT</button> <button>DELETE</button></td>
+                    </tr>";
+
+                }
+                ?>
+
+
+
+
+
+
+        </thead>
+    </table>
+    </div>
+</body>
+</html>
